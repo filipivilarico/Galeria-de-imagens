0e15213072f04543e4568f2a78204ea18d705f79
FILIPI VILARICO DA SILVA <21193366@CFP127ALUNO.LOCAL>
2021-10-05 16:52:58 -0300

    oi

--- aula09/script.js
+++ aula09/script.js
@@ -1,7 +1,68 @@
 "use strict"
 
-//<a href="#jm1" class="galeria-itens">
-//<img src="img/jm1.jpeg" alt="">  
-//</a>
+const imagens = [
+   "https://static9.depositphotos.com/1020341/1109/i/600/depositphotos_11094645-stock-photo-map-of-brazil-with-waving.jpg",
+    "./img/jm1.jpeg",
+    "./img/jm2.png",
+    "./img/jm3.jpg",
+    "./img/jm4.jpg",
+    "./img/jm5.jpg",
+    "./img/jm6.jpg",
+    "./img/jm7.jpg",
+    "./img/jm8.jpg"
 
-carregarGaleria()
+
+]
+
+const filtrarId = (urlImagem) => {
+    const ultimaBarra = urlImagem.lastIndexOf("/") +1
+    const ultimoPonto = urlImagem.lastIndexOf(".")
+    return urlImagem.substring(ultimaBarra,ultimoPonto)
+}
+const criarItem = (urlImagem) => {
+    const container = document.querySelector(".container")
+    const novoLink = document.createElement("a")
+    novoLink.href= `#${filtrarId(urlImagem)}`
+    novoLink.classList.add("galeria-itens")
+    novoLink.innerHTML = `<img src="${urlImagem}" alt="">`
+    container.appendChild(novoLink)
+}
+
+const criarSlide = (urlImagem, indice, array) => {
+    const container = document.querySelector(".slide-container")
+    const novaDiv = document.createElement("div")
+    novaDiv.classList.add("slide")
+    novaDiv.id = filtrarId(urlImagem)
+
+
+    const indiceAnterior = indice == 0 ? array.length - 1 : indice - 1
+    const slideAnterior = filtrarId(array[indiceAnterior])
+
+    const indiceSeguinte = indice == array.length - 1 ? 0 : indice + 1
+    const slideSeguinte = filtrarId(array[indiceSeguinte])
+
+
+
+    novaDiv.innerHTML =`
+    
+        <div class="imagem-container">
+     <a href="#" class="fechar">&#10006;</a>
+    <a href="#${slideAnterior}" class="navegacao anterior">&#171;</a>
+     <img src="${urlImagem}" alt="">
+     <a href="#${slideSeguinte}" class="navegacao proximo">&#187;</a>
+    </div>
+        `
+
+        container.appendChild(novaDiv)
+        
+}
+
+
+
+const carregarGaleria = (imgs) => imgs.forEach(criarItem)
+const carregarslide = (imgs) => imgs.forEach(criarSlide)
+
+carregarGaleria(imagens)
+
+carregarslide(imagens)
+
